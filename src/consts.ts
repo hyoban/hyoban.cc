fb7089b148776d21525c5e298d288b6a40b43946
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-01-01 11:21:03 +0800

    chore: update

--- src/consts.ts
+++ src/consts.ts
@@ -4,4 +4,3 @@
 export const SITE_URL = 'https://hyoban.cc'
 export const SITE_TITLE = 'Hyoban'
 export const SITE_DESCRIPTION = 'Don\'t do what you should do, do you want.'
-export const SITE_ICON = 'https://avatars.githubusercontent.com/u/38493346?v=4'
