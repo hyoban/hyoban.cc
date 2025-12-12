a0b51b0e4181efdec9a0658690b73f824e2ea1b4
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-12-12 21:18:59 +0800

    chore: rss link

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -10,7 +10,7 @@ export async function GET(context) {
     site: context.site,
     items: posts.map(post => ({
       ...post.data,
-      link: `/posts/${post.data.link}`,
+      link: `/${post.data.link}`,
     })),
   })
 }
