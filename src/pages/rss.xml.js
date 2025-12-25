790a51ade5241d888a9caf49d9ebb5f5d66f769b
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-12-25 23:19:26 +0800

    feat: full content

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,18 +1,22 @@
 import rss from '@astrojs/rss'
 import { SITE_DESCRIPTION, SITE_TITLE } from '@/consts'
-import { getSortedPosts } from '@/utils'
 
 export async function GET(context) {
-  const posts = await getSortedPosts()
+  const postImportResult = import.meta.glob('../content/posts/**/*.md', { eager: true })
+  const posts = Object.values(postImportResult)
+
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: posts.map(post => ({
-      ...post.data,
-      link: `/${post.data.link}`,
-      customData: `<guid>${post.data.link}</guid>`,
-    })),
+    items: await Promise.all(
+      posts.map(async post => ({
+        ...post.frontmatter,
+        link: `/${post.frontmatter.link}`,
+        customData: `<guid>${post.frontmatter.link}</guid>`,
+        content: (await post.compiledContent()),
+      })),
+    ),
     trailingSlash: false,
   })
 }
